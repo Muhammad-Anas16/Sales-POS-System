23e3898a4c77a67f8fe2494d415084e6ec187ca1
Muhammad-Anas16 <anas.hanf38@gmail.com>
2026-03-31 17:56:10 +0500

    trying to install Shad/CN UI in it

--- src/renderer/src/components/Header.jsx
+++ src/renderer/src/components/Header.jsx
@@ -1,6 +1,8 @@
-import { Wifi } from 'lucide-react'
+import { Wifi, WifiOff, Clock } from 'lucide-react'
 
 function Versions() {
+  const isOnline = true
+
   return (
     <header className="fixed bg-[#FFFFFF] flex justify-between items-center px-8 h-16 w-full">
       <div className="flex items-center gap-4">
@@ -10,10 +12,15 @@ function Versions() {
       </div>
       <div className="flex items-center gap-6">
         <div className="hidden md:flex items-center gap-1 text-sm font-medium text-stone-500">
+          <Clock className="text-[#396662] font-bold size-3.5" />
           <span>08:42 AM</span>
         </div>
         <div className="flex items-center gap-2">
-          <Wifi className="text-[#396662] font-bold size-5" />
+          {isOnline ? (
+            <Wifi className="text-[#396662] font-bold size-5" />
+          ) : (
+            <WifiOff className="text-[#396662] font-bold size-5" />
+          )}
         </div>
       </div>
     </header>
--- src/renderer/src/pages/HomePage.jsx
+++ src/renderer/src/pages/HomePage.jsx
@@ -3,13 +3,13 @@ import Headers from '../components/Header'
 
 const HomePage = () => {
   return (
-    <div>
-      <div>
+    <div className="h-screen w-screen bg-[#F4E7DF]">
         <Headers />
+      <div className=''>
         <h1 className="text-3xl text-green-500 font-bold">Home Page</h1>
       </div>
     </div>
   )
 }
 
-export default HomePage;
+export default HomePage
